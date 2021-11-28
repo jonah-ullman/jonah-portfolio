33ce8d1855a46f939a623dadaf17d10f470230b6
Jonah Ullman <jonahmullman@gmail.com>
2021-11-28 15:13:25 -0600

    Initial html structure for nav

--- components/Nav/nav.js
+++ components/Nav/nav.js
@@ -1,9 +1,20 @@
 import styles from './button.module.scss';
+import Button from '../Button/button.js';
 
 function Nav() {
+  const resumeButtonConfig = {
+    label: 'Resume'
+  };
+
   return (
     <nav>
       <div className={styles.logo}>LOGO</div>
+      <ol className={styles.navContainer}>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Resume</a></li>
+      </ol>
     </nav>
   );
 }
